6174c5c69fd8324aa5e3dc0a76188c474bb0949a
Meiwang1248 <meiwang1248@hotmail.com>
2021-12-06 16:22:44 -0800

    Updated App.js with HomeScreen

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from './logo.svg';
+import {BrowserRouter, Link, Switch, Route} from "react-router-dom";
+import React from 'react';
 import './App.css';
+import HomeScreen from "./components/home-screen";
 
 function App() {
-
+    return (
+        <div className="container-fluid">
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" exact = {true}>
+                        <HomeScreen/>
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
