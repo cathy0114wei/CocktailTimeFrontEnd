c6a58fd1cb543584cdd9627691b6f35c8f4bd277
LumingYang <yang.lum@northeastern.edu>
2021-12-07 18:37:48 -0600

    add search function

---
+++ src/components/search/search-screen.js
@@ -0,0 +1,29 @@
+import React, {useState, useEffect} from 'react'
+import {Link, useParams, useHistory} from "react-router-dom";
+import cocktailService from "../../services/cocktail-service"
+import './search.css'
+
+const SearchScreen = () => {
+    const history = useHistory();
+    const {cocktailName} = useParams()
+    const [searchCocktail, setSearchCocktail] = useState(cocktailName)
+    const [results, setResults] = useState({drinks: []})
+    useEffect(() => {
+        if (cocktailName !== "undefined" && typeof cocktailName !== "undefined") {
+            setSearchCocktail(cocktailName)
+            findCocktailByName(cocktailName)
+        }
+    }, [])
+
+    const findCocktailByName = (cocktailName) => {
+        history.push(`/search/${cocktailName}`)
+        cocktailService.findCocktailByName(cocktailName)
+            .then((results) => {
+                setResults(results)
+            })
+    }
+    return (
+
+    )
+}
+export default SearchScreen
