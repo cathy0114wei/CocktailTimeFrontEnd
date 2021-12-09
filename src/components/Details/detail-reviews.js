e91d6cefee8d47f0f7713062b6ee2b94ec370dd3
Cathy wei <cathywei@CathydeMacBook-Pro.local>
2021-12-09 10:34:32 -0800

    detail-reviews draft

--- src/components/Details/detail-reviews.js
+++ src/components/Details/detail-reviews.js
@@ -21,9 +21,65 @@ const ReviewList = ({cocktailId, user, cocktailName, cocktailImg}) => {
     const submitHandler = () => {
         reviewService.createReviewForCocktail(cocktailId, myReview.textArea, user.username, cocktailName, cocktailImg, user._id)
             .then(res => console.log(res))
-
         setReview(review => [...review, myReview])
     }
+    return (
+        <div>
+            <h5 className= "section-title">
+                Reviews
+            </h5>
 
+            <ul>
+                {
+                    review && review[0] && review.map((item, i) => {
+                        return(
+                            <li className="list-spacing"
+                                key={i}>
+                                <Link className="reviews-title" to={(user && user._id && item.userId && user._id === item.userId) ?
+                                    "/profile" : `/profiles/${item.userId}`}>
+                                    {item.username}
+                                </Link>
+                                <div className="reviews-text">
+                                    {item.textArea}
+                                </div>
+
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+            {
+                !user &&
+                <>
+                    <div className='alert alert-warning'>
+                        Please login to submit your review.
+                    </div>
+                </>
+            }
+            {
+                user &&
+                <>
+                    <h5 className="separation-padding">
+                        Submit Your Review
+                    </h5>
+                    <div>
+                        <textarea id="reviewText" placeholder="Please enter here."
+                                  value={myReview.textArea}
+                                  onChange={(e) => {
+                                      setMyReview({...myReview, textArea: e.target.value, username: user.username});
+                                  }}
+                                  className="form-control"/>
+                    </div>
+                    <br/>
+                    <div className="d-grid gap-2 d-md-block">
+                        <button className = "btn btn-primary"
+                                onClick={submitHandler}>
+                            Submit
+                        </button>
+                    </div>
+                </>
+            }
+        </div>
+    )
 }
 export default ReviewList
