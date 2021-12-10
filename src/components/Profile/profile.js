59bed78e2ae025f4e16fadfb3d69eca3a081c4c6
Meiwang1248 <meiwang1248@hotmail.com>
2021-12-09 16:03:12 -0800

    Updated profile.js

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Fragment} from 'react';
 import ProfileBio from "./profile-bio";
 import ProfileAbout from "./profile-about";
 import ProfileFavorite from "./profile-favorite";
@@ -6,9 +6,13 @@ import ProfileReview from "./profile-review";
 import {findProfileById} from "../../services/profile-service";
 import {Link, useParams} from "react-router-dom";
 
-const Profile = ({user}) => {
+const Profile = ({user, setUser}) => {
+
     const {uid} = useParams();
+    const [profileType, setProfileType] = useState("About");
     const [currentProfile, setCurrentProfile] = useState({});
+
+
 
     const isAdmin = user && user.role === "ADMIN";
     const adminId = isAdmin && user._id;
@@ -17,15 +21,14 @@ const Profile = ({user}) => {
 
     useEffect(() => {
         if(uid) {
-            findProfileById(uid)
+            profileService.findProfileById(uid)
                 .then((profile) => {
                     setCurrentProfile(profile)
                 })
         }
     }, [uid])
     return (
-        <>
-            <h1>Profile</h1>
+        <Fragment>
             <h1 className='rose-red'>Profile </h1>
             {
                 isAdmin && <Link className="btn btn-primary" to="/profiles" style={{margin:"10px"}}>
@@ -40,7 +43,62 @@ const Profile = ({user}) => {
                 <Link className='btn btn-outline-primary' to='/login'>Back to login page</Link>
             </>
             }
-        </>
+            {
+                (user || uid) &&
+                <>
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="profile-bio col-md-4">
+                                <ProfileBio user={user} setUser={setUser} saveProfile={saveProfile} editable={editable} currentProfile={currentProfile}/>
+                                <div className="list-group col-md-10">
+                                    <button type="button"
+                                            className="list-group-item"
+                                            name="bio"
+                                            onClick={(e) => setProfileType("About")}>
+                                        About Me
+                                    </button>
+                                    <button type="button"
+                                            className="list-group-item"
+                                            name="bio"
+                                            onClick={(e) => setProfileType("Review")}>
+                                        My Reviews
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div className="col-md-8">
+                                {
+                                    editable &&
+                                    <h2 className="rose-red">{user.firstName} {user.lastName}'s Profile</h2>
+                                }
+                                {
+                                    !editable &&
+                                    <h2>{currentProfile.firstName} {currentProfile.lastName}'s Profile</h2>
+                                }
+                                <div className="row">
+                                    <div className="col-md-8">
+                                        {
+                                            profileType === "About" &&
+                                            <ProfileAbout user={user} setUser={setUser} saveProfile={saveProfile} editable={editable} currentProfile={currentProfile}/>
+                                        }
+                                        {
+                                            profileType === "Review" &&
+                                            <ProfileReview user={user} editable={editable} currentProfile={currentProfile}/>
+                                        }
+                                    </div>
+                                    <div className="col-md-4">
+
+                                        <ProfileFavorite user={user} editable={editable} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </>
+            }
+        </Fragment>
+    )
     )
 }
 export default Profile;
