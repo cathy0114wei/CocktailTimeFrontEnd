5b7264879a144bad331c1e9fc3283af870ece590
Cathy wei <cathywei@CathydeMacBook-Pro.local>
2021-12-11 15:08:19 -0800

    favorite-service

---
+++ src/services/favorite-service.js
@@ -0,0 +1,38 @@
+const USER_URL = "http://localhost:3001/api";
+
+export const isFavorite = (cocktailId, userId) =>
+    fetch(`${USER_URL}/favorite/${cocktailId}/${userId}`)
+        .then(res => res.json());
+
+export const findFavoritesByUserId = (userId) =>
+    fetch(`${USER_URL}/favorite/${userId}`)
+        .then(res => res.json());
+
+export const addFavorite = (cocktailId, userId, username, cocktailName, cocktailImg) =>
+    fetch(`${USER_URL}/favorite`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({cocktailId, userId, username, cocktailName, cocktailImg})
+    })
+        .then(res => res.json());
+
+export const removeFavorite = (cocktailId, userId) =>
+    fetch(`${USER_URL}/favorite`, {
+        method: 'DELETE',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify({cocktailId, userId})
+    })
+        .then(res => res.json());
+
+const api = {
+    isFavorite,
+    findFavoritesByUserId,
+    addFavorite,
+    removeFavorite
+}
+
+export default api;
