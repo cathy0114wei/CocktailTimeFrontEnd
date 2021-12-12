e0651f880a90783dc23c59fd43a75487330d8e62
Meiwang1248 <meiwang1248@hotmail.com>
2021-12-11 22:13:55 -0800

    Corrected profile-about.js

--- src/components/Profile/profile-about.js
+++ src/components/Profile/profile-about.js
@@ -1,17 +1,40 @@
-import React, {useState} from 'react';
+import React, {useState} from 'react'
 
-const ProfileBio = () => {
+const ProfileAbout = ({user, setUser, saveProfile, editable, currentProfile}) => {
+
     const [editing, setEditing] = useState(false);
+
     return (
         <>
-            <div className="bio-block">
-                <ul className="list-group">
-                    <li className="list-group-item">{currentProfile.phone}</li>
-                    <li className="list-group-item">{currentProfile.email}</li>
-                    <li className="list-group-item">{currentProfile.address}</li>
-                </ul>
-            </div>
+            <h3 className="rose-red">About Me</h3>
+            {
+                editable &&
+                <>
+                    {
+                        editing &&
+                        <div className="about-block">
+                            <i onClick={() => {saveProfile(user); setEditing(false)}} className="fa fa-check" style={{color:"green"}}></i>
+                            <textarea rows={10} defaultValue={user.about} onChange={(e) => setUser(user =>
+                                ({...user, about: e.target.value}))} className="form-control"/>
+                        </div>
+                    }
+                    {
+                        !editing &&
+                        <div className="about-block">
+                            <i onClick={() => setEditing(true)} className="fa fa-edit" style={{color:"blue"}}></i>
+                            <p className="text-justify">{user.about}</p>
+                        </div>
+                    }
+                </>
+            }
+            {
+                !editable &&
+                <div className="about-block">
+                    <p className="text-justify">{currentProfile.about}</p>
+                </div>
+            }
         </>
-    );
+    )
 }
-export default ProfileBio;
+
+export default ProfileAbout;
