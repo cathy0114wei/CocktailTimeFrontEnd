4d95e9548c2e9e753363d6878da1e68bd13f11da
Cathy wei <cathywei@CathydeMacBook-Pro.local>
2021-12-13 21:51:21 -0800

    privacy policy

---
+++ src/components/PrivacyPolicy/privacy.js
@@ -0,0 +1,165 @@
+import "./privacy.css"
+
+const Privacy = () => {
+    return (
+        <>
+            <h2>Privacy Policy</h2>
+            <p>Last updated: December 13, 2021</p>
+            <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your
+                information when You use the Service and tells You about Your privacy rights and how the law protects
+                You.</p>
+            <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the
+                collection and use of information in accordance with this Privacy Policy. </p>
+
+            <h3>Collecting and Using Your Personal Data</h3>
+            <h4>Types of Data Collected</h4>
+            <h5>Personal Data</h5>
+            <p>While using Our Service, We may ask You to provide Us with certain personally identifiable information
+                that
+                can be used to contact or identify You. Personally identifiable information may include, but is not
+                limited
+                to:</p>
+            <ul>
+                <li>
+                    <p>Email</p>
+                </li>
+                <li>
+                    <p>First name and last name</p>
+                </li>
+                <li>
+                    <p>Phone number</p>
+                </li>
+                <li>
+                    <p>Address</p>
+                </li>
+                <li>
+                    <p>Usage Data</p>
+                </li>
+            </ul>
+            <h5>Usage Data</h5>
+            <p>Usage Data is collected automatically when using the Service.</p>
+            <p>Usage Data may include information such as Your Device's Internet Protocol address (e.g. IP address),
+                browser
+                type, browser version, the pages of our Service that You visit, the time and date of Your visit, the
+                time
+                spent on those pages, unique device identifiers and other diagnostic data.</p>
+            <p>When You access the Service by or through a mobile device, We may collect certain information
+                automatically,
+                including, but not limited to, the type of mobile device You use, Your mobile device unique ID, the IP
+                address of Your mobile device, Your mobile operating system, the type of mobile Internet browser You
+                use,
+                unique device identifiers and other diagnostic data.</p>
+            <p>We may also collect information that Your browser sends whenever You visit our Service or when You access
+                the
+                Service by or through a mobile device.</p>
+
+            <h4>Use of Your Data</h4>
+            <p>The Company may use Personal Data for the following purposes:</p>
+            <ul>
+                <li>
+                    <p><strong>To provide and maintain our Service</strong>, including to monitor the usage of our
+                        Service.
+                    </p>
+                </li>
+                <li>
+                    <p><strong>To manage Your Account:</strong> to manage Your registration as a user of the Service.
+                        The
+                        Personal Data You provide can give You access to different functionalities of the Service that
+                        are
+                        available to You as a registered user.</p>
+                </li>
+                <li>
+                    <p><strong>For the performance of a contract:</strong> the development, compliance and undertaking
+                        of
+                        the purchase contract for the products, items or services You have purchased or of any other
+                        contract with Us through the Service.</p>
+                </li>
+                <li>
+                    <p><strong>To contact You:</strong> To contact You by email, telephone calls, SMS, or other
+                        equivalent
+                        forms of electronic communication, such as a mobile application's push notifications regarding
+                        updates or informative communications related to the functionalities, products or contracted
+                        services, including the security updates, when necessary or reasonable for their implementation.
+                    </p>
+                </li>
+                <li>
+                    <p><strong>To provide You</strong> with news, special offers and general information about other
+                        goods,
+                        services and events which we offer that are similar to those that you have already purchased or
+                        enquired about unless You have opted not to receive such information.</p>
+                </li>
+                <li>
+                    <p><strong>To manage Your requests:</strong> To attend and manage Your requests to Us.</p>
+                </li>
+                <li>
+                    <p><strong>For business transfers:</strong> We may use Your information to evaluate or conduct a
+                        merger,
+                        divestiture, restructuring, reorganization, dissolution, or other sale or transfer of some or
+                        all of
+                        Our assets, whether as a going concern or as part of bankruptcy, liquidation, or similar
+                        proceeding,
+                        in which Personal Data held by Us about our Service users is among the assets transferred.</p>
+                </li>
+                <li>
+                    <p><strong>For other purposes</strong>: We may use Your information for other purposes, such as data
+                        analysis, identifying usage trends, determining the effectiveness of our promotional campaigns
+                        and
+                        to evaluate and improve our Service, products, services, marketing and your experience.</p>
+                </li>
+            </ul>
+            <p>We may share Your personal information in the following situations:</p>
+            <ul>
+                <li><strong>With Service Providers:</strong> We may share Your personal information with Service
+                    Providers
+                    to monitor and analyze the use of our Service, to contact You.
+                </li>
+                <li><strong>For business transfers:</strong> We may share or transfer Your personal information in
+                    connection with, or during negotiations of, any merger, sale of Company assets, financing, or
+                    acquisition of all or a portion of Our business to another company.
+                </li>
+                <li><strong>With Affiliates:</strong> We may share Your information with Our affiliates, in which case
+                    we
+                    will require those affiliates to honor this Privacy Policy. Affiliates include Our parent company
+                    and
+                    any other subsidiaries, joint venture partners or other companies that We control or that are under
+                    common control with Us.
+                </li>
+                <li><strong>With business partners:</strong> We may share Your information with Our business partners to
+                    offer You certain products, services or promotions.
+                </li>
+                <li><strong>With other users:</strong> when You share personal information or otherwise interact in the
+                    public areas with other users, such information may be viewed by all users and may be publicly
+                    distributed outside.
+                </li>
+                <li><strong>With Your consent</strong>: We may disclose Your personal information for any other purpose
+                    with
+                    Your consent.
+                </li>
+            </ul>
+
+            <h3>Links to Other Websites</h3>
+            <p>Our Service may contain links to other websites that are not operated by Us. If You click on a third
+                party
+                link, You will be directed to that third party's site. We strongly advise You to review the Privacy
+                Policy
+                of every site You visit.</p>
+            <p>We have no control over and assume no responsibility for the content, privacy policies or practices of
+                any
+                third party sites or services.</p>
+            <h3>Changes to this Privacy Policy</h3>
+            <p>We may update Our Privacy Policy from time to time. We will notify You of any changes by posting the new
+                Privacy Policy on this page.</p>
+            <p>We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming
+                effective and update the "Last updated" date at the top of this Privacy Policy.</p>
+            <p>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy
+                Policy
+                are effective when they are posted on this page.</p>
+            <h3>Contact Us</h3>
+            <p>If you have any questions about this Privacy Policy, You can contact us:</p>
+            <ul>
+                <li>By email: gorgeousCocktail@webdev.com</li>
+            </ul>
+
+        </>)
+}
+export default Privacy;
